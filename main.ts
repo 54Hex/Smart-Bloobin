06ee1144a8cbcd83cf9c7067d4df488787e31877
WX <73157658+54Hex@users.noreply.github.com>
2022-06-14 15:12:28 +0800

    brand new line of code

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@
  * 
  * Number 3: Food waste
  */
+OLED.init(128, 64)
 pins.digitalWritePin(DigitalPin.P1, 1)
 basic.showIcon(IconNames.Angry)
 huskylens.initI2c()
@@ -17,39 +18,48 @@ basic.pause(100)
 huskylens.writeName(2, "Plastic")
 basic.showIcon(IconNames.Square)
 basic.forever(function () {
+    music.setVolume(100)
     huskylens.request()
-    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showLeds(`
-                . . # . .
-                . # . # .
-                . . . # .
-                . . # . .
-                . . # . .
-                `)
-        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Yes)
-            basic.showNumber(1)
-            basic.pause(1000)
-            basic.clearScreen()
-        } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Yes)
-            basic.showNumber(1)
-            basic.pause(1000)
-            basic.clearScreen()
-        } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Yes)
-            basic.showNumber(3)
-            basic.pause(1000)
-            basic.clearScreen()
-        } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Yes)
-            basic.showNumber(3)
-            basic.pause(1000)
-            basic.clearScreen()
-        } else {
-            basic.showIcon(IconNames.No)
-            basic.clearScreen()
-        }
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        OLED.writeStringNewLine("Ready")
+        basic.pause(5000)
+        OLED.clear()
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . . # .
+            . . # . .
+            . . # . .
+            `)
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+        basic.clearScreen()
+        basic.showNumber(1)
+        basic.pause(1000)
+        basic.clearScreen()
+        OLED.writeStringNewLine("Plastic")
+        basic.pause(5000)
+        OLED.clear()
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Yes)
+        basic.showNumber(1)
+        basic.pause(5000)
+        basic.clearScreen()
+    } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Yes)
+        basic.showNumber(3)
+        basic.pause(5000)
+        basic.clearScreen()
+    } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        basic.showIcon(IconNames.Yes)
+        basic.showNumber(3)
+        basic.pause(5000)
+        basic.clearScreen()
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.clearScreen()
+        music.playTone(131, music.beat(BeatFraction.Whole))
+        music.stopAllSounds()
     }
 })
