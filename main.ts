7f77a3090186ccc3870eb4d7103715b2af24a1fb
WX <73157658+54Hex@users.noreply.github.com>
2022-06-14 18:31:10 +0800

    edited order.

--- main.ts
+++ main.ts
@@ -31,47 +31,44 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
-    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        basic.pause(1000)
-        basic.clearScreen()
-        OLED.writeStringNewLine("Does it contain any liquid? Press A (Yes) or B (No)")
-        basic.pause(8000)
-        OLED.clear()
-        if (input.buttonIsPressed(Button.A)) {
-            OLED.writeStringNewLine("Unfortunately this cannot be recyclable ")
-            basic.pause(8000)
-            OLED.clear()
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            OLED.writeStringNewLine("This can be recyclable!")
-            basic.pause(8000)
-            OLED.clear()
-            OLED.writeStringNewLine("Plastic")
-            basic.showNumber(1)
+    } else {
+        if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Yes)
             basic.pause(1000)
             basic.clearScreen()
+            OLED.writeStringNewLine("Does it contain any liquid? Press A (Yes) or B (No)")
+            basic.pause(8000)
             OLED.clear()
+            if (input.buttonIsPressed(Button.A)) {
+                OLED.writeStringNewLine("Unfortunately this cannot be recyclable ")
+                basic.pause(8000)
+                OLED.clear()
+            }
+            if (input.buttonIsPressed(Button.B)) {
+                OLED.writeStringNewLine("This can be recyclable!")
+                basic.pause(8000)
+                OLED.clear()
+                OLED.writeStringNewLine("Plastic")
+                basic.showNumber(1)
+                basic.pause(1000)
+                basic.clearScreen()
+                OLED.clear()
+            }
+        } else {
+            if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+                OLED.writeStringNewLine("Ready")
+                basic.pause(5000)
+                OLED.clear()
+                basic.showLeds(`
+                    . . # . .
+                    . # . # .
+                    . . . # .
+                    . . # . .
+                    . . # . .
+                    `)
+            }
+            basic.showIcon(IconNames.No)
+            basic.clearScreen()
         }
-    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        basic.showNumber(1)
-        basic.pause(5000)
-        basic.clearScreen()
-    } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        basic.showNumber(3)
-        basic.pause(5000)
-        basic.clearScreen()
-    } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        basic.showNumber(3)
-        basic.pause(5000)
-        basic.clearScreen()
-    } else {
-        basic.showIcon(IconNames.No)
-        basic.clearScreen()
-        music.playTone(131, music.beat(BeatFraction.Whole))
-        music.stopAllSounds()
     }
 })
