cca4fe070eefdbe4004bb2f5a4eb99a2b3167bc0
WX <73157658+54Hex@users.noreply.github.com>
2022-06-15 14:39:19 +0800

    added servo

--- main.ts
+++ main.ts
@@ -7,11 +7,22 @@
  * 
  * Number 3: Food waste
  */
+function closeShutter () {
+    pins.servoWritePin(AnalogPin.P0, 0)
+}
 input.onButtonPressed(Button.A, function () {
     OLED.newLine()
     OLED.writeStringNewLine("Unfortunately this cannot be recyclable ")
     basic.pause(8000)
     OLED.clear()
+})
+input.onButtonPressed(Button.AB, function () {
+    OLED.writeStringNewLine("Shutter open")
+    basic.pause(8000)
+    OLED.clear()
+    openShutter()
+    basic.pause(5000)
+    closeShutter()
 })
 input.onButtonPressed(Button.B, function () {
     OLED.newLine()
@@ -24,6 +35,9 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     OLED.clear()
 })
+function openShutter () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+}
 OLED.init(128, 64)
 basic.showIcon(IconNames.Angry)
 huskylens.initI2c()
@@ -51,25 +65,12 @@ basic.forever(function () {
         if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
             basic.showIcon(IconNames.Yes)
             basic.pause(1000)
-            OLED.writeStringNewLine("Does it contain any liquid? Press A (Yes) or B (No)")
+            OLED.writeStringNewLine("Does it contain any waste? Press A (Yes) or B (No)")
             basic.pause(8000)
             basic.clearScreen()
             OLED.clear()
-        } else {
-            if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-                OLED.writeStringNewLine("Ready")
-                basic.pause(5000)
-                OLED.clear()
-                basic.showLeds(`
-                    . . # . .
-                    . # . # .
-                    . . . # .
-                    . . # . .
-                    . . # . .
-                    `)
-            }
-            basic.showIcon(IconNames.No)
-            basic.clearScreen()
         }
+        basic.showIcon(IconNames.No)
+        basic.clearScreen()
     }
 })
